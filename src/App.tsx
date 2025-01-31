7fc1d625cf1ad86833d280b99751c871c56a072e
Smahle <stianmahle@gmail.com>
2025-01-31 08:51:10 +0000

    navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
+  const pages = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' }
+  ];
+
   return (
     <Router>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page.name}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                    component={Link} // Button behaves like a Link now
+                    to={page.path} 
+                  >
+                    {page.name}
+                  </Button>
+                ))}
+              </Box>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
