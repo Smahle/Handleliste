2e59f132b946057a0cb7aee179b299c58cdaa948
Smahle <stianmahle@gmail.com>
2025-02-05 23:48:07 +0100

    Update useCreateCart.tsx

--- src/hooks/useCreateCart.tsx
+++ src/hooks/useCreateCart.tsx
@@ -1,5 +1,15 @@
 
 
-export default function useCreateCart(){
+export default function useCreateCart(setCarts:()=>void, setActiveCartId:()=>void){
+    const createCart = () => {
+    const name = prompt("Enter cart name")?.trim();
+    if (!name) return;
 
+    const newCart = { id: crypto.randomUUID(), name, products: [], owner:user };
+    
+return{
+    setCarts((prev) => [...prev, newCart]);
+    setActiveCartId(newCart.id);
+  };
+}
 }
