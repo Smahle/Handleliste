7df25a1844449ef5af9a4ab6dc7596e1e0a2d835
Smahle <stianmahle@gmail.com>
2025-02-14 15:22:01 +0000

    Discover doubleclick working

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,8 +1,15 @@
 import ProductSearch from "../components/ProductSearch";
 
 export default function Discover(){
-
+    const handleProductDoubleClick = (product: Product) => {
+        console.log(product.name)
+    };
+  
     return (
-        <ProductSearch/>
+      <div>
+        <div>
+          <ProductSearch onDoubleClick={handleProductDoubleClick} />
+        </div>
+     </div>
     )
 }
