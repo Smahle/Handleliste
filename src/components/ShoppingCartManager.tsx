0acee9c18c4245f0b37a0f2e976f5f52ed9e3891
Smahle <stianmahle@gmail.com>
2025-02-17 09:52:59 +0000

    routing to owner of cart

--- src/components/ShoppingCartManager.tsx
+++ src/components/ShoppingCartManager.tsx
@@ -2,11 +2,12 @@ import { Button, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import ShoppingCart from "./ShoppingCart";
 import styles from "./ShoppingCartManager.module.css";
 import { useCartContext } from "../context/CartContext";
+import { useNavigate } from "react-router-dom";
 
 export default function ShoppingCartManager() {
   const {createNewCart, activeCartId, carts, setActiveCartId, copyCart} = useCartContext();
   const activeCart = carts.find((cart) => cart.id === activeCartId);
-
+  const navigate = useNavigate()
   const handleChange = (event: SelectChangeEvent)=>{
     setActiveCartId(event.target.value)
   }
@@ -21,7 +22,9 @@ export default function ShoppingCartManager() {
            <MenuItem key={cart.id} value={cart.id}>{cart.name}</MenuItem>
           ))}
         </Select>
-        <p style={{ paddingLeft: "1rem" }}>Cart owner: {activeCart?.owner} </p>       
+        <p style={{ paddingLeft: "1rem" }}>Cart owner:
+          <Button disabled={!activeCart} onClick={()=>navigate(`/profile/${activeCart?.owner}`)} style={{ paddingLeft: "1rem" }}> {activeCart?.owner} </Button>           
+       </p>       
      </div>
 
       <div className={styles.shoppingCart}>
